9fce15438bbb8a143be6540b0ab468630fb1f664
DESKTOP-A65QN8P\ABHI <adodd028@uottawa.ca>
2017-10-23 17:54:37 -0400

    Basic REST WebService

---
+++ src/com/musicBonanza/WebServicesClient/LoginPOJO.java
@@ -0,0 +1,20 @@
+package com.musicBonanza.WebServicesClient;
+
+public class LoginPOJO {
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	String username;
+	String password;
+
+}
