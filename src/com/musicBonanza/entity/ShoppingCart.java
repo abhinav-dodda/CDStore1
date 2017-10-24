48fdb906e159dc7bfa39a26ea2c779185d014467
NANDHINI\Nandhini <nbabu043@uottawa.ca>
2017-10-24 18:49:15 -0400

    shopping cart entity

---
+++ src/com/musicBonanza/entity/ShoppingCart.java
@@ -0,0 +1,29 @@
+package com.musicBonanza.entity;
+
+public class ShoppingCart {
+	
+	private int ProductId;
+	private String ProductName;
+	private float ProdPrice;
+	public int getProductId() {
+		return ProductId;
+	}
+	public void setProductId(int productId) {
+		ProductId = productId;
+	}
+	public String getProductName() {
+		return ProductName;
+	}
+	public void setProductName(String productName) {
+		ProductName = productName;
+	}
+	public float getProdPrice() {
+		return ProdPrice;
+	}
+	public void setProdPrice(float prodPrice) {
+		ProdPrice = prodPrice;
+	}
+	
+	
+
+}
