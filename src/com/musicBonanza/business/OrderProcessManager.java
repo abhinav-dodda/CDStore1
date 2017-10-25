ad6eeeb9dc83d8bbb10c6a8728fdbd7bb9edb93f
ramansarao1992 <ramansarao1992@gmail.com>
2017-10-25 13:04:11 -0400

    get account

---
+++ src/com/musicBonanza/business/OrderProcessManager.java
@@ -0,0 +1,33 @@
+package com.musicBonanza.business;
+
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.musicBonanza.dao.OrderProcessDao;
+import com.musicBonanza.entity.PurchaseOrder;
+import com.musicBonanza.entity.Shipping;
+import com.musicBonanza.entity.User;
+
+public class OrderProcessManager {
+	
+	/*public int createOrder(int[] shoppingCartInfo, PurchaseOrder purchaseOrder, Shipping shippingInfo)
+	{
+	OrderProcessDao orderProcessDao = new OrderProcessDao();
+	
+	PurchaseOrder purchaseorder = new PurchaseOrder();
+	purchaseorder.setShippings(shippings);
+	purchaseorder.setpurchaseOrder(purchaseOrder);
+	purchaseorder.setshippingInfo(shippingInfo);
+	
+	return orderProcessDao.createOrder(purchaseorder);
+	}*/
+	
+	
+	
+	
+	
+}
---
+++ src/com/musicBonanza/dao/OrderProcessDao.java
@@ -0,0 +1,10 @@
+package com.musicBonanza.dao;
+
+public class OrderProcessDao {
+
+	public static int createOrder(OrderProcessDao purchaseorder) {
+		
+		return 0;
+	}
+
+}
