5b18f430e9b0e637b2daa93221fb992e4e26251e
NANDHINI\Nandhini <nbabu043@uottawa.ca>
2017-10-29 20:55:03 -0400

    updated product category with cart changes

--- src/com/musicBonanza/controller/ProductCategory.java
+++ src/com/musicBonanza/controller/ProductCategory.java
@@ -2,14 +2,19 @@ package com.musicBonanza.controller;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.core.MediaType;
 
+import com.musicBonanza.entity.CD;
 import com.musicBonanza.utils.Constants;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
@@ -88,7 +93,38 @@ public class ProductCategory extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String category = request.getParameter("selectCategory");
-
+        //Naveen yet to get product details from DB for cart session
+		
+		//Nandhini : created to get the product details to set the session attribute
+		
+		String[] productName = {"Naanum Rowdy Dhaan", "OK Kanmani", "AR Rahuman Mashups"};
+		String[] productId = {"product-1", "product-2", "product-3"};
+		String[] prodCat = {"Tamil", "Multilingual", "Regional"};
+		float[] prodPrice = {45, 55, 105};
+		int size = productId.length;
+		HttpSession session = request.getSession();
+		List<CD> cart = new ArrayList<CD>();
+		//pass the parameter to the class and set the array list values
+		for (int i = 0; i < size; i++)
+		{
+		CD usercart = new CD();
+		usercart.setProductId(productId[i]);
+		usercart.setProductName(productName[i]);
+		usercart.setProdCat(prodCat[i]);
+		usercart.setProdPrice(prodPrice[i]);
+		//store it in the Arraylist	
+		cart.add(usercart);
+		//cart.add(i, usercart);
+		//System.out.println(cart.get(i));
+		//cart.add(usercart);
+		}
+		session.setAttribute("cart",cart);
+		
+	   //forward the request back to product page
+	   request.getRequestDispatcher("ProductCategories.jsp").forward(request, response);
+		 
+	   //Nandhini :end of the code
+		 
 		Client client = Client.create();
 		WebResource webResource = client.resource(Constants.localhostUrl + "ProductCategory");
 		ClientResponse webServiceResponse = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
