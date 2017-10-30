a00e9a4f705638f7f1bcb7f461b8e4989a1976a2
DESKTOP-A65QN8P\ABHI <adodd028@uottawa.ca>
2017-10-29 20:26:10 -0400

    Removed unwanted files and gave documentation for methods in DBManager

--- src/com/musicBonanza/Helper/Helper.java
+++ src/com/musicBonanza/Helper/Helper.java
@@ -10,25 +10,30 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.*;
 
 public class Helper {
-	
+	/*
+	 * This method is used to configure a JDBC Connection Pooling and read all the
+	 * SQL Files from a properties file
+	 * 
+	 * @param path of the sqlQuery.Properties file
+	 */
+
 	public static Properties LoadProperty(String propertyFileName) {
 
 		Properties property = new Properties();
 		ClassLoader loader = Thread.currentThread().getContextClassLoader();
 		InputStream inputStream;
 		try {
-		inputStream = loader.getResourceAsStream(propertyFileName);
-			
-					property.load(inputStream);
-				System.out.println("Prop File loaded");
-				
-		}
-		 catch (IOException e) {
-			// log.error(e.getLocalizedMessage(), e);
+			inputStream = loader.getResourceAsStream(propertyFileName);
+
+			property.load(inputStream);
+			System.out.println("Prop File loaded");
+
+		} catch (IOException e) {
 			e.printStackTrace();
-		 }
+		}
 		return property;
 	}
+
 	/**
 	 * Fetches the property value when property key is passed.
 	 *
@@ -38,7 +43,7 @@ public class Helper {
 	 */
 	public static String FetchPropertyAndProcessQuery(Properties propertyObj, String propertyKey,
 			List<?> parameterList) {
-		
+
 		String rawQuery = StringUtils.EMPTY;
 		String processedQuery = StringUtils.EMPTY;
 
@@ -55,6 +60,7 @@ public class Helper {
 
 		return processedQuery;
 	}
+
 	/**
 	 * Processes the query string by replacing @param parameters with actual
 	 * parameters.
@@ -72,23 +78,26 @@ public class Helper {
 			String result = (String) iterator.next();
 			System.out.println(result);
 			processedString = processedString.replace("?" + i, result);
-			System.out.println(processedString);
-			System.out.println("Reached ProcessQuery");
 			i++;
 		}
 
 		return processedString;
 	}
-	 /* Loads the property file using the below parameters.
+
+	/*
+	 * Loads the property file using the below parameters.
 	 *
 	 * @param propertyKey
+	 * 
 	 * @param className
-	 * @return 
+	 * 
+	 * @return
+	 * 
 	 * @return
 	 */
 	public static void main(String[] args) {
 
-	 Helper.LoadProperty("com/musicBonanza/Util/sqlQuery.properties");
-	
-}
+		Helper.LoadProperty(Constants.sqlQueryPropertiesFilePath);
+
+	}
 }
